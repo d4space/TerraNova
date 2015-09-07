eb597b2d68f91856ee39adb45e9c92aa0e2fd7a2
sangilpark <sangilpark@knu.ac.kr>
2015-09-07 20:42:05 +0900

    haha

--- NtupleMaker/python/pf2pat_template_MC_cfg.py
+++ NtupleMaker/python/pf2pat_template_MC_cfg.py
@@ -1,6 +1,31 @@
 import FWCore.ParameterSet.Config as cms 
+
+## MessageLogger
+from FWCore.MessageLogger.MessageLogger_cfi import *
+#process.load("Configuration.StandardSequences.Geometry_cff")
+#from Configuration.StandardSequences.Geometry_cff import *
+from Configuration.Geometry.GeometryIdeal_cff import *
+from Configuration.StandardSequences.FrontierConditions_GlobalTag_cff import *
+from Configuration.StandardSequences.MagneticField_cff import *
+
+#from Configuration.PyReleaseValidation.autoCond import autoCond
+#process.GlobalTag.globaltag = cms.string( autoCond[ 'startup' ] )
+
 #PF2PAT
 from PhysicsTools.PatAlgos.patSequences_cff import *
+
+goodOfflinePrimaryVertices = cms.EDFilter("VertexSelector",
+   src = cms.InputTag("offlinePrimaryVertices"),
+   cut = cms.string("!isFake && ndof > 4 && abs(z) < 24 && position.Rho < 2"), # tracksSize() > 3 for the older cut
+   filter = cms.bool(True),   # otherwise it won't filter the events, just produce an empty vertex collection.
+)
+
+noscraping = cms.EDFilter("FilterOutScraping",
+   applyfilter = cms.untracked.bool(True),
+   debugOn = cms.untracked.bool(False),
+   numtrack = cms.untracked.uint32(10),
+   thresh = cms.untracked.double(0.25)
+)
 
 
 acceptedElectrons = cms.EDFilter("PATElectronSelector",
@@ -114,6 +139,11 @@ patMuEleTauFilter = cms.EDFilter("MultiObjectCountFilter",
 #    process.eidVeryLooseMC * process.eidLooseMC * process.eidMediumMC
 #  * process.eidTightMC * process.eidSuperTightMC * process.eidHyperTight1MC
 #)
+
+from CommonTools.RecoAlgos.HBHENoiseFilter_cfi import *
+HBHENoiseFilter.minNumIsolatedNoiseChannels = cms.int32(9999)
+HBHENoiseFilter.minIsolatedNoiseSumE = cms.double(9999)
+HBHENoiseFilter.minIsolatedNoiseSumEt = cms.double(9999)
 
 
 ##################################################################
